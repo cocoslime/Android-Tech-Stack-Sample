4f9b75a316827dee7aca1910ab79f78d3c35a865
dongmen94 <dongmen94@gmail.com>
2024-10-06 20:34:00 +0900

    create RecyclerViewActivity

---
+++ app/src/main/java/com/cocoslime/presentation/recyclerview/RecyclerViewActivity.kt
@@ -0,0 +1,93 @@
+package com.cocoslime.presentation.recyclerview
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.activity.ComponentActivity
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.cocoslime.presentation.databinding.ActivityRecyclerViewBinding
+import com.cocoslime.presentation.databinding.ItemRecyclerViewEntryBinding
+import com.cocoslime.presentation.databinding.ItemRecyclerViewHeaderBinding
+
+class RecyclerViewActivity: ComponentActivity() {
+
+    private var _binding: ActivityRecyclerViewBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        _binding = ActivityRecyclerViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+
+    }
+
+    private class Adapter(
+
+    ): ListAdapter<ListItem, BindingViewHolder<out ListItem, *>>(
+        ListItemCallback()
+    ) {
+
+        override fun getItemViewType(position: Int): Int {
+            return when (getItem(position)) {
+                is ListItem.Header -> ViewType.HEADER.ordinal
+                is ListItem.Entry -> ViewType.ENTRY.ordinal
+                is ListItem.Footer -> ViewType.FOOTER.ordinal
+                else -> throw NotImplementedError()
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BindingViewHolder<out ListItem, *> {
+            return when(viewType) {
+                ViewType.HEADER.ordinal -> HeaderViewHolder(parent)
+                else -> throw NotImplementedError()
+            }
+        }
+
+        override fun onBindViewHolder(holder: BindingViewHolder<out ListItem, *>, position: Int) {
+            val item = getItem(position)
+            holder.onBind(item)
+        }
+
+        private class HeaderViewHolder(
+            parent: ViewGroup,
+        ): BindingViewHolder<ListItem.Header, ItemRecyclerViewHeaderBinding>(
+            binding = ItemRecyclerViewHeaderBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        ) {
+            override fun onBind(item: ListItem.Header) = Unit
+        }
+
+        private class EntryViewHolder(
+            parent: ViewGroup,
+        ): BindingViewHolder<ListItem.Entry, ItemRecyclerViewEntryBinding>(
+            binding = ItemRecyclerViewEntryBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        ) {
+            override fun onBind(item: ListItem.Entry) {
+
+                binding.contents.text = item.content
+            }
+        }
+
+        enum class ViewType(
+            val poolSize: Int = 5
+        ) {
+            HEADER(1),
+            ENTRY,
+            FOOTER(1),
+        }
+    }
+}
