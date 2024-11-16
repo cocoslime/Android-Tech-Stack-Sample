256612e014e05d4e8294a6936ed9c897772d0dc7
dongmen94 <dongmen94@gmail.com>
2024-11-16 12:22:06 +0900

    네트워크 모델 수정

--- app/src/main/java/com/cocoslime/data/model/GithubRepoResponse.kt
+++ app/src/main/java/com/cocoslime/data/model/GithubRepoResponse.kt
@@ -8,5 +8,12 @@ data class GithubRepoResponse(
     val id: Long,
     val name: String,
     @SerialName("html_url") val url: String,
-    val description: String,
+    val description: String?,
+    val language: String?,
+    val owner: Owner,
+)
+
+@Serializable
+data class Owner(
+    @SerialName("avatar_url") val avatarUrl: String,
 )
