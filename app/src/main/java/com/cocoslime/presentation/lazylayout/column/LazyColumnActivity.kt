b9b1807aa17a2eaf5c27fc6b21777774d44f9fbd
dongmen94 <dongmen94@gmail.com>
2024-11-17 20:08:17 +0900

    Debug 모드 LazyLayout 알림

--- app/src/main/java/com/cocoslime/presentation/lazylayout/column/LazyColumnActivity.kt
+++ app/src/main/java/com/cocoslime/presentation/lazylayout/column/LazyColumnActivity.kt
@@ -1,10 +1,12 @@
 package com.cocoslime.presentation.lazylayout.column
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.MaterialTheme
+import com.cocoslime.presentation.BuildConfig
 
 class LazyColumnActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +17,14 @@ class LazyColumnActivity: ComponentActivity() {
             MaterialTheme {
                 LazyColumnScreen()
             }
+        }
+
+        if (BuildConfig.DEBUG) {
+            Toast.makeText(
+                this,
+                "Debug mode 에서는 LazyLayout 의 성능 이슈가 있을 수 있다.",
+                Toast.LENGTH_LONG
+            ).show()
         }
     }
 }
