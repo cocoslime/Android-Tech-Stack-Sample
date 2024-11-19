7127c21ecb5d5361482be5b0390c20cc39001251
dongmen94 <dongmen94@gmail.com>
2024-11-19 21:46:01 +0900

    implement TestGithubService

---
+++ app/src/test/kotlin/com/cocoslime/presentation/data/service/TestGithubService.kt
@@ -0,0 +1,31 @@
+package com.cocoslime.presentation.data.service
+
+import com.cocoslime.data.model.GithubRepoResponse
+import com.cocoslime.data.model.Owner
+import com.cocoslime.data.service.GithubService
+
+private const val MAX_SIZE = 100
+
+class TestGithubService : GithubService {
+
+    override suspend fun getRepos(
+        username: String,
+        perPage: Int,
+        page: Int
+    ): List<GithubRepoResponse> {
+        val startIndex = (page - 1) * perPage
+        val endIndex = minOf(startIndex + perPage, MAX_SIZE)
+        return (startIndex until endIndex).map { index ->
+            GithubRepoResponse(
+                id = index.toLong(),
+                name = "Repo $index",
+                url = "",
+                description = null,
+                language = null,
+                owner = Owner(
+                    avatarUrl = ""
+                )
+            )
+        }
+    }
+}
