2f4df071b6441ad9faba1c2fcd957337867523dc
dongmen94 <dongmen94@gmail.com>
2024-11-21 22:22:25 +0900

    assertEquals 테스트 완성

--- app/src/test/kotlin/com/cocoslime/presentation/paging/PagingViewModelTest.kt
+++ app/src/test/kotlin/com/cocoslime/presentation/paging/PagingViewModelTest.kt
@@ -2,16 +2,16 @@ package com.cocoslime.presentation.paging
 
 
 import androidx.paging.PagingData
+import androidx.paging.testing.asSnapshot
 import com.cocoslime.data.model.GithubRepoResponse
 import com.cocoslime.presentation.TestDispatcherRule
 import com.cocoslime.presentation.data.service.TestGithubService
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import kotlin.test.assertEquals
 
 class PagingViewModelTest {
 
@@ -28,7 +28,7 @@ class PagingViewModelTest {
     }
 
     @Test
-    fun test_items_contain_one_to_ten() = runTest {
+    fun test_items_contain_0_to_50() = runTest {
         // Get the Flow of PagingData from the ViewModel under test
         val items: Flow<PagingData<GithubRepoResponse>> = viewModel.repoPagingFlow
 
@@ -42,8 +42,8 @@ class PagingViewModelTest {
         // With the asSnapshot complete, you can now verify that the snapshot
         // has the expected values
         assertEquals(
-            expected = (0..50).map(Int::toString),
-            actual = itemsSnapshot
+            expected = (0 until 50).map(TestGithubService::createFakeGithubRepoResponse),
+            actual = itemsSnapshot.subList(0, 50)
         )
     }
 }
