b74f55fdf72378743f6ec42dd15e388250ae83c9
dongmen94 <dongmen94@gmail.com>
2024-12-02 23:22:18 +0900

    test_footer_is_visible

--- app/src/main/java/com/cocoslime/presentation/paging/PagingViewModel.kt
+++ app/src/main/java/com/cocoslime/presentation/paging/PagingViewModel.kt
@@ -4,13 +4,16 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import androidx.paging.insertSeparators
 import androidx.paging.map
+import com.cocoslime.data.model.GithubRepoResponse
 import com.cocoslime.data.paging.GithubRepoPagingSource
 import com.cocoslime.data.service.GithubService
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.flatMapLatest
@@ -25,7 +28,7 @@ class PagingViewModel @Inject constructor (
     val user: StateFlow<String> = _user
 
     @OptIn(ExperimentalCoroutinesApi::class)
-    val repoPagingFlow = user.flatMapLatest {
+    val repoPagingFlow: Flow<PagingData<UiModel>> = user.flatMapLatest {
         Pager(
             // Configure how data is loaded by passing additional properties to
             // PagingConfig, such as prefetchDistance.
@@ -44,15 +47,12 @@ class PagingViewModel @Inject constructor (
             .flow
     }
         .map { pagingData ->
-            pagingData.map {
-                UiModel.UserModel(
-                    id = it.id,
-                    name = it.name,
-                    url = it.url,
-                    description = it.description,
-                    language = it.language,
-                )
-            }
+            pagingData
+                .map {
+                    UiModel.UserModel.fromDataModel(
+                        it
+                    )
+                }
                 .insertSeparators<UiModel.UserModel, UiModel> { before, after ->
                     when {
                         before == null -> UiModel.SeparatorModel("HEADER")
@@ -75,7 +75,19 @@ class PagingViewModel @Inject constructor (
             val url: String,
             val description: String?,
             val language: String?,
-        ): UiModel
+        ): UiModel {
+            companion object {
+                fun fromDataModel(
+                    data: GithubRepoResponse
+                ) = UserModel(
+                    id = data.id,
+                    name = data.name,
+                    url = data.url,
+                    description = data.description,
+                    language = data.language,
+                )
+            }
+        }
 
         data class SeparatorModel(
             val description: String
--- app/src/test/kotlin/com/cocoslime/presentation/paging/PagingViewModelTest.kt
+++ app/src/test/kotlin/com/cocoslime/presentation/paging/PagingViewModelTest.kt
@@ -4,9 +4,9 @@ package com.cocoslime.presentation.paging
 import androidx.paging.PagingData
 import androidx.paging.testing.asSnapshot
 import com.cocoslime.common.createFakeGithubRepoResponse
-import com.cocoslime.data.model.GithubRepoResponse
 import com.cocoslime.presentation.TestDispatcherRule
 import com.cocoslime.presentation.data.service.TestGithubService
+import com.cocoslime.presentation.paging.PagingViewModel.UiModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
@@ -29,7 +29,7 @@ class PagingViewModelTest {
         viewModel = PagingViewModel(
             githubService = TestGithubService().apply {
                 repoResponseList.addAll(
-                    (0 until 100).map { index ->
+                    (0 until MAX_RESPONSE_SIZE).map { index ->
                         createFakeGithubRepoResponse(index.toLong())
                     }
                 )
@@ -40,9 +40,9 @@ class PagingViewModelTest {
     @Test
     fun test_items_contain_0_to_50() = runTest {
         // Get the Flow of PagingData from the ViewModel under test
-        val items: Flow<PagingData<GithubRepoResponse>> = viewModel.repoPagingFlow
+        val items: Flow<PagingData<PagingViewModel.UiModel>> = viewModel.repoPagingFlow
 
-        val itemsSnapshot: List<GithubRepoResponse> = items.asSnapshot {
+        val itemsSnapshot: List<PagingViewModel.UiModel> = items.asSnapshot {
             // Scroll to the 50th item in the list. This will also suspend till
             // the prefetch requirement is met if there's one.
             // It also suspends until all loading is complete.
@@ -52,8 +52,36 @@ class PagingViewModelTest {
         // With the asSnapshot complete, you can now verify that the snapshot
         // has the expected values
         assertEquals(
-            expected = (0L until 50L).map(::createFakeGithubRepoResponse),
-            actual = itemsSnapshot.subList(0, 50)
+            expected = (0L until 50L)
+                .map(::createFakeGithubRepoResponse)
+                .map { UiModel.UserModel.fromDataModel(it) },
+            actual = itemsSnapshot
+                .filterIsInstance<UiModel.UserModel>()
+                .subList(0, 50)
         )
+    }
+
+    @Test
+    fun test_footer_is_visible() = runTest {
+        // Get the Flow of PagingData from the ViewModel under test
+        val items: Flow<PagingData<UiModel>> = viewModel.repoPagingFlow
+
+        val itemsSnapshot: List<UiModel> = items.asSnapshot {
+            // Scroll till the footer is visible
+            appendScrollWhile { item: UiModel ->
+                item is UiModel.UserModel || (item is UiModel.SeparatorModel && item.description != "FOOTER")
+            }
+        }
+
+        assertEquals(
+            expected = MAX_RESPONSE_SIZE,
+            actual = itemsSnapshot
+                .filterIsInstance<UiModel.UserModel>()
+                .size
+        )
+    }
+
+    companion object {
+        private const val MAX_RESPONSE_SIZE = 100
     }
 }
