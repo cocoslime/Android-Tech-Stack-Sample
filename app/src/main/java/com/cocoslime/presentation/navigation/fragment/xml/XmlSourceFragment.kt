d73a12e955b478c5b78efe804b7f750e7bc67f08
dongmen94 <dongmen94@gmail.com>
2025-04-13 22:37:37 +0900

    collectAsStateWithLifecycle

--- app/src/main/java/com/cocoslime/presentation/navigation/fragment/xml/XmlSourceFragment.kt
+++ app/src/main/java/com/cocoslime/presentation/navigation/fragment/xml/XmlSourceFragment.kt
@@ -5,18 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.fragment.findNavController
 import com.cocoslime.presentation.navigation.fragment.component.SourceScreen
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.map
 
 class XmlSourceFragment : Fragment() {
 
@@ -32,22 +30,17 @@ class XmlSourceFragment : Fragment() {
 
             setContent {
                 MaterialTheme {
-                    var otherMessage by remember {
-                        mutableStateOf("")
-                    }
-
-                    LaunchedEffect(Unit) {
-                        otherMessage = findNavController().currentBackStackEntry?.savedStateHandle
-                            ?.getStateFlow(
-                                key = XmlDestinationFragment.KEY_RESULT,
-                                initialValue = ""
-                            )
-                            ?.value ?: ""
-                    }
+                    val message by findNavController().currentBackStackEntry?.savedStateHandle
+                        ?.getStateFlow(
+                            key = XmlDestinationFragment.KEY_RESULT,
+                            initialValue = ""
+                        )
+                        ?.map { if (it.isEmpty()) "Destination(Empty)" else "넘어온 값: $it" }
+                        ?.collectAsStateWithLifecycle(initialValue = "Destination(Init)")
+                        ?: remember { mutableStateOf("Destination(null)") }
 
                     SourceScreen(
-                        message = "Test",
-                        otherMessage = otherMessage,
+                        message = message,
                         navigateToDestination = {
                             findNavController().navigate(
                                 XmlSourceFragmentDirections.actionSourceFragmentToDestinationFragment(
